d68de6b9bec17f3527f6a7d3561af438b144140e
Edmundo Henrique de Paiva Silva <edmundo.henrique@geb.inatel.br>
2022-06-03 15:16:09 -0300

    Deixando a fila pública

--- TimeDeFutebol/src/main/java/br/inatel/TimeDeFutebol/Main.java
+++ TimeDeFutebol/src/main/java/br/inatel/TimeDeFutebol/Main.java
@@ -1,17 +1,28 @@
 package br.inatel.TimeDeFutebol;
-import br.inatel.TimeDeFutebol.Fila.Fila;
+import br.inatel.TimeDeFutebol.Jogador.Jogador;
 import br.inatel.TimeDeFutebol.Posições.*;
 
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) {
-        Fila fila = new Fila();
+        Queue<Object> listaJogadores = new LinkedList<Object>();
 
         Goleiro gl = new Goleiro("Ornstein", "Goleiro");
         Zagueiro zg = new Zagueiro("Velstadt", "atacante");
         Lateral lt = new Lateral("Nameless King", "Lateral");
         MeioDeCampo mc = new MeioDeCampo("Gascoigne", "Meio de Campo");
-        Atacante at = new Atacante("Radagon", "atacante");
+        Atacante at = new Atacante("Radagon", "Atacante");
 
+        listaJogadores.add(gl);
+        listaJogadores.add(zg);
+        listaJogadores.add(lt);
+        listaJogadores.add(mc);
+        listaJogadores.add(at);
 
-}}
+        while(listaJogadores.isEmpty()){
+            listaJogadores.element();
+        }
+    }
+}
